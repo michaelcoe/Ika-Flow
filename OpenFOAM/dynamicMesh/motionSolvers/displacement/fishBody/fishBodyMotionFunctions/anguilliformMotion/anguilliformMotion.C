a7f3a27058d34b4361f4487b60888abbc1a72108
michaelcoe <michael.coe85@gmail.com>
2021-08-25 14:54:34 +1200

    added motion solvers

---
+++ OpenFOAM/dynamicMesh/motionSolvers/displacement/fishBody/fishBodyMotionFunctions/anguilliformMotion/anguilliformMotion.C
@@ -0,0 +1,130 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2016 OpenFOAM Foundation
+    Copyright (C) 2019 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "anguilliformMotion.H"
+#include "addToRunTimeSelectionTable.H"
+#include "unitConversion.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace fishBodyMotionFunctions
+{
+    defineTypeNameAndDebug(anguilliformMotion, 0);
+    addToRunTimeSelectionTable
+    (
+        fishBodyMotionFunction,
+        anguilliformMotion,
+        dictionary
+    );
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::fishBodyMotionFunctions::anguilliformMotion::
+anguilliformMotion
+(
+    const dictionary& SBMFCoeffs,
+    const Time& runTime
+)
+:
+    fishBodyMotionFunction(SBMFCoeffs, runTime)
+{
+    read(SBMFCoeffs);
+}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+Foam::septernion
+Foam::fishBodyMotionFunctions::anguilliformMotion::
+transformation() const
+{
+    scalar t = time_.value();
+
+    vector eulerAngles = amplitude_*sin(omega_*t);
+
+    // Convert the rotational motion from deg to rad
+    eulerAngles *= degToRad();
+
+    quaternion R(quaternion::XYZ, eulerAngles);
+    septernion TR(septernion(-origin_)*R*septernion(origin_));
+
+    DebugInFunction << "Time = " << t << " transformation: " << TR << endl;
+
+    return TR;
+}
+
+Foam::tmp<Foam::pointField>
+Foam::fishBodyMotionFunctions::anguilliformMotion::
+transformationPoints(pointField& p0) const
+{
+    scalar tm = time_.value();
+
+    forAll(p0, pointI)
+    {
+    	const scalar x = (p0[pointI].component(0)-origin_[0])/length_;
+        const scalar y = p0[pointI].component(1)-origin_[1];
+        const scalar z = p0[pointI].component(2)-origin_[2];
+
+        // new value by equation
+        const scalar localAmplitude = amplitude_[0]*exp(alpha_*(x - 1));
+        const scalar dampFactor = 0.5-0.5*tanh(ramp_*x-(ramp_+9));
+                    
+        const scalar yr = y + localAmplitude * dampFactor * sin((waveNumber_ * x) - (omega_ * tm)) * length_;
+        
+        p0[pointI] = vector(x, yr, z);
+    }
+
+    return p0;
+}
+
+
+bool Foam::fishBodyMotionFunctions::anguilliformMotion::read
+(
+    const dictionary& SBMFCoeffs
+)
+{
+    fishBodyMotionFunction::read(SBMFCoeffs);
+
+    SBMFCoeffs_.readEntry("origin", origin_);
+    SBMFCoeffs_.readEntry("amplitude", amplitude_);
+    SBMFCoeffs_.readEntry("waveNumber", waveNumber_);
+    SBMFCoeffs_.readEntry("length", length_);
+    SBMFCoeffs_.readEntry("ramp", ramp_);
+    SBMFCoeffs_.readEntry("omega", omega_);
+    SBMFCoeffs_.readEntry("alpha", alpha_);
+
+    return true;
+}
+
+
+// ************************************************************************* //
---
+++ OpenFOAM/dynamicMesh/motionSolvers/displacement/fishBody/fishBodyMotionSolver.C
@@ -0,0 +1,190 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2016 OpenFOAM Foundation
+    Copyright (C) 2020 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fishBodyMotionSolver.H"
+#include "addToRunTimeSelectionTable.H"
+#include "transformField.H"
+#include "cellZoneMesh.H"
+#include "cellSet.H"
+#include "boolList.H"
+#include "syncTools.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(fishBodyMotionSolver, 0);
+    addToRunTimeSelectionTable
+    (
+        motionSolver,
+        fishBodyMotionSolver,
+        dictionary
+    );
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::fishBodyMotionSolver::fishBodyMotionSolver
+(
+    const polyMesh& mesh,
+    const IOdictionary& dict
+)
+:
+    points0MotionSolver(mesh, dict, typeName),
+    SBMFPtr_(fishBodyMotionFunction::New(coeffDict(), mesh.time())),
+    pointIDs_(),
+    moveAllCells_(false)
+{
+    word cellZoneName =
+        coeffDict().getOrDefault<word>("cellZone", "none");
+
+    word cellSetName =
+        coeffDict().getOrDefault<word>("cellSet", "none");
+
+    if ((cellZoneName != "none") && (cellSetName != "none"))
+    {
+        FatalIOErrorInFunction(coeffDict())
+            << "Either cellZone OR cellSet can be supplied, but not both. "
+            << "If neither is supplied, all cells will be included"
+            << exit(FatalIOError);
+    }
+
+    labelList cellIDs;
+    if (cellZoneName != "none")
+    {
+        Info<< "Applying fish body motion to cellZone " << cellZoneName
+            << endl;
+
+        label zoneID = mesh.cellZones().findZoneID(cellZoneName);
+
+        if (zoneID == -1)
+        {
+            FatalErrorInFunction
+                << "Unable to find cellZone " << cellZoneName
+                << ".  Valid cellZones are:"
+                << mesh.cellZones().names()
+                << exit(FatalError);
+        }
+
+        cellIDs = mesh.cellZones()[zoneID];
+    }
+
+    if (cellSetName != "none")
+    {
+        Info<< "Applying fish body motion to cellSet " << cellSetName
+            << endl;
+
+        cellSet set(mesh, cellSetName);
+
+        cellIDs = set.toc();
+    }
+
+    label nCells = returnReduce(cellIDs.size(), sumOp<label>());
+    moveAllCells_ = nCells == 0;
+
+    if (moveAllCells_)
+    {
+        Info<< "Applying fish body motion to entire mesh" << endl;
+    }
+    else
+    {
+        // collect point IDs of points in cell zone
+
+        boolList movePts(mesh.nPoints(), false);
+
+        forAll(cellIDs, i)
+        {
+            label celli = cellIDs[i];
+            const cell& c = mesh.cells()[celli];
+            forAll(c, j)
+            {
+                const face& f = mesh.faces()[c[j]];
+                forAll(f, k)
+                {
+                    label pointi = f[k];
+                    movePts[pointi] = true;
+                }
+            }
+        }
+
+        syncTools::syncPointList(mesh, movePts, orEqOp<bool>(), false);
+
+        DynamicList<label> ptIDs(mesh.nPoints());
+        forAll(movePts, i)
+        {
+            if (movePts[i])
+            {
+                ptIDs.append(i);
+            }
+        }
+
+        pointIDs_.transfer(ptIDs);
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::fishBodyMotionSolver::~fishBodyMotionSolver()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::pointField> Foam::fishBodyMotionSolver::curPoints() const
+{
+    if (moveAllCells_)
+    {
+        return transformPoints(SBMFPtr_().transformation(), points0_);
+    }
+    else
+    {
+        tmp<pointField> ttransformedPts(new pointField(mesh().points()));
+        pointField& transformedPts = ttransformedPts.ref();
+        pointField p0(points0_, pointIDs_);
+
+
+        UIndirectList<point>(transformedPts, pointIDs_) = SBMFPtr_().transformationPoints
+        (
+            p0
+        );
+
+        /*
+        UIndirectList<point>(transformedPts, pointIDs_) = transformPoints
+        (
+            SBMFPtr_().transformation(),
+            pointField(points0_, pointIDs_)
+        );
+        */
+        return ttransformedPts;
+    }
+}
+
+
+// ************************************************************************* //
